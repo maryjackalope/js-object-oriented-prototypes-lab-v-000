238f34c6d672725bf42bf5c3de82a1a63ddd7994
Mary Jack <maryjackinthebox@gmail.com>
2018-11-18 21:21:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,23 +3,23 @@
   this.homeState = homeState
   this.training = training
   
-   this.veto = function(){
+   this.prototype.veto = function(){
     return "No, I must disagree";
     }
     
-    this.approve = function(){
+    this.prototype.approve = function(){
     return "You can do that!";
     }
     
-    this.doCharity = function(){
+    this.prototype.doCharity = function(){
       return "I like to help people."
     }
     
-    this.releasePressStatement = function(){
+    this.prototype.releasePressStatement = function(){
       return "You will see great things from Scuber."
     }
     
-    this.sayHi = function(){
+    this.prototype.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
     }
 }
